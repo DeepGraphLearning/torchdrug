24eb73ffa9dd7d8ed50525fe8b2eaf8511ce3840
Zhaocheng Zhu <healmysoul@163.com>
2021-09-10 15:36:57 +0800

    add variadic_randperm

--- torchdrug/layers/functional/__init__.py
+++ torchdrug/layers/functional/__init__.py
@@ -1,6 +1,6 @@
 from .functional import multinomial, masked_mean, mean_with_nan, shifted_softplus, multi_slice_mask, as_mask, \
     _size_to_index, _extend, variadic_log_softmax, variadic_softmax, variadic_sum, variadic_mean, variadic_max, \
-    variadic_cross_entropy, variadic_sort, variadic_topk, variadic_arange, one_hot, \
+    variadic_cross_entropy, variadic_sort, variadic_topk, variadic_arange, variadic_randperm, one_hot, \
     clipped_policy_gradient_objective, policy_gradient_objective
 from .embedding import transe_score, distmult_score, complex_score, simple_score, rotate_score
 from .spmm import generalized_spmm, generalized_rspmm
@@ -8,7 +8,7 @@ from .spmm import generalized_spmm, generalized_rspmm
 __all__ = [
     "multinomial", "masked_mean", "mean_with_nan", "shifted_softplus", "multi_slice_mask", "as_mask",
     "variadic_log_softmax", "variadic_softmax", "variadic_sum", "variadic_mean", "variadic_max",
-    "variadic_cross_entropy", "variadic_sort", "variadic_topk", "variadic_arange", "one_hot",
+    "variadic_cross_entropy", "variadic_sort", "variadic_topk", "variadic_arange", "variadic_randperm", "one_hot",
     "clipped_policy_gradient_objective", "policy_gradient_objective",
     "transe_score", "distmult_score", "complex_score", "simple_score", "rotate_score",
     "generalized_spmm", "generalized_rspmm",

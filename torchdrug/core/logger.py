e9b60bf6d51f99a5d77569f2e68973627608a0c0
Manan Goel <manangoel1999@gmail.com>
2022-02-23 13:16:48 +0000

    Added more arguments to customize the wandb run and added handling to check if a wandb run is already initialized and warn the user accordlingly

--- torchdrug/core/logger.py
+++ torchdrug/core/logger.py
@@ -1,5 +1,6 @@
 import pprint
 import logging
+import warnings
 
 from torchdrug.core import Registry as R
 from torchdrug.utils import pretty
@@ -70,18 +71,29 @@ class WandbLogger(ConsoleLogger):
         dir (str, optional): path to save wandb outputs. By default, outputs are stored in ``./wandb``.
     """
 
-    def __init__(self, project=None, name=None, dir=None, **kwargs):
+    def __init__(self, project=None, name=None, dir=None, entity=None, config=None, **kwargs):
         super(WandbLogger, self).__init__()
         try:
             import wandb
         except ModuleNotFoundError:
             raise ModuleNotFoundError("Wandb is not found. Please install it with `pip install wandb`")
 
-        self.run = wandb.init(project=project, name=name, dir=dir, reinit=True, **kwargs)
+        if wandb.run is not None:
+            warnings.warn(
+                 "There is a wandb run already in progress and newly created instances of `WandbLogger` will reuse"
+                " this run. If this is not desired, call `wandb.finish()` before instantiating `WandbLogger`."
+            )
+            self.run = wandb.run
+        else:
+            self.run = wandb.init(project=project, name=name, dir=dir, entity=entity, reinit=True, **kwargs)
 
-        self.run.define_metric("train/batch/*", step_metric="batch", summary="none")
-        for split in ["train", "valid", "test"]:
-            self.run.define_metric("%s/epoch/*" % split, step_metric="epoch")
+        if getattr(self.run, "define_metric", None):
+            self.run.define_metric("train/batch/*", step_metric="batch", summary="none")
+            for split in ["train", "valid", "test"]:
+                self.run.define_metric("%s/epoch/*" % split, step_metric="epoch")
+        
+        if config:
+            self.run.config.update(config)
 
     def log(self, record, step_id, category="train/batch"):
         super(WandbLogger, self).log(record, step_id, category)

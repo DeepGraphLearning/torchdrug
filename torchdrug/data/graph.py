99479500c2a6401b21f268c8b177425a08b8107f
Zhaocheng Zhu <zhaochengzhu@pku.edu.cn>
2022-04-23 18:12:17 -0400

    Update graph.py

--- torchdrug/data/graph.py
+++ torchdrug/data/graph.py
@@ -1219,7 +1219,7 @@ class PackedGraph(Graph):
 
         index = self._standarize_index(index[0], self.batch_size)
         count = index.bincount(minlength=self.batch_size)
-        if len(count) > 0 and max(count) > 1:
+        if self.batch_size > 0 and count.max() > 1:
             graph = self.repeat_interleave(count)
             index_order = index.argsort()
             order = torch.zeros_like(index)

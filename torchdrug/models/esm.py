a959f68f0c19f368be9e380f5a587de6970b3c67
Zuobai Zhang <17300240035@fudan.edu.cn>
2023-04-24 21:23:53 -0400

    Include ESM-2 models in esm.py

--- torchdrug/models/esm.py
+++ torchdrug/models/esm.py
@@ -30,19 +30,48 @@ class EvolutionaryScaleModeling(nn.Module, core.Configurable):
         "ESM-1v": "https://dl.fbaipublicfiles.com/fair-esm/models/esm1v_t33_650M_UR90S_1.pt",
         "ESM-1b-regression":
             "https://dl.fbaipublicfiles.com/fair-esm/regression/esm1b_t33_650M_UR50S-contact-regression.pt",
+        "ESM-2-8M": "https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t6_8M_UR50D.pt",
+        "ESM-2-35M": "https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t12_35M_UR50D.pt",
+        "ESM-2-150M": "https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t30_150M_UR50D.pt",
+        "ESM-2-650M": "https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t33_650M_UR50D.pt",
+        "ESM-2-3B": "https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t36_3B_UR50D.pt",
+        "ESM-2-15B": "https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t48_15B_UR50D.pt",
     }
 
     md5 = {
         "ESM-1b": "ba8914bc3358cae2254ebc8874ee67f6",
         "ESM-1v": "1f04c2d2636b02b544ecb5fbbef8fefd",
         "ESM-1b-regression": "e7fe626dfd516fb6824bd1d30192bdb1",
+        "ESM-2-8M": "8039fc9cee7f71cd2633b13b5a38ff50",
+        "ESM-2-35M": "a894ddb31522e511e1273abb23b5f974",
+        "ESM-2-150M": "229fcf8f9f3d4d442215662ca001b906",
+        "ESM-2-650M": "ba6d997e29db07a2ad9dca20e024b102",
+        "ESM-2-3B": "d37a0d0dbe7431e48a72072b9180b16b",
+        "ESM-2-15B": "af61a9c0b792ae50e244cde443b7f4ac",
     }
 
     output_dim = {
         "ESM-1b": 1280,
         "ESM-1v": 1280,
+        "ESM-2-8M": 320,
+        "ESM-2-35M": 480,
+        "ESM-2-150M": 640,
+        "ESM-2-650M": 1280,
+        "ESM-2-3B": 2560,
+        "ESM-2-15B": 5120,
     }
 
+    num_layer = {
+        "ESM-1b": 33,
+        "ESM-1v": 33,
+        "ESM-2-8M": 6,
+        "ESM-2-35M": 12,
+        "ESM-2-150M": 30,
+        "ESM-2-650M": 33,
+        "ESM-2-3B": 36,
+        "ESM-2-15B": 48,
+    }
+    
     max_input_length = 1024 - 2
 
     def __init__(self, path, model="ESM-1b", readout="mean"):
@@ -57,6 +86,7 @@ class EvolutionaryScaleModeling(nn.Module, core.Configurable):
         self.output_dim = self.output_dim[model]
         self.model = _model
         self.alphabet = alphabet
+        self.repr_layer = self.num_layer[model]
         self.register_buffer("mapping", mapping)
 
         if readout == "sum":
@@ -71,7 +101,7 @@ class EvolutionaryScaleModeling(nn.Module, core.Configurable):
             raise ValueError("Unknown model `%s`" % model)
         model_file = utils.download(self.url[model], path, md5=self.md5[model])
         model_data = torch.load(model_file, map_location="cpu")
-        if model != "ESM-1v":
+        if model != "ESM-1v" and not model.startswith("ESM-2"):
             regression_model = "%s-regression" % model
             regression_file = utils.download(self.url[regression_model], path, md5=self.md5[regression_model])
             regression_data = torch.load(regression_file, map_location="cpu")
@@ -122,8 +152,8 @@ class EvolutionaryScaleModeling(nn.Module, core.Configurable):
             input, size_ext = functional._extend(input, size_ext, eos, torch.ones_like(size_ext))
         input = functional.variadic_to_padded(input, size_ext, value=self.alphabet.padding_idx)[0]
 
-        output = self.model(input, repr_layers=[33])
-        residue_feature = output["representations"][33]
+        output = self.model(input, repr_layers=[self.repr_layer])
+        residue_feature = output["representations"][self.repr_layer]
 
         residue_feature = functional.padded_to_variadic(residue_feature, size_ext)
         starts = size_ext.cumsum(0) - size_ext
